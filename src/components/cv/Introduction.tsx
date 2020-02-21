2ebd3c523bfd2c1ca55d54dee70c43ab40eb3ef4
CHARDEVEL AXEL <axel.chardevel.partner@decathlon.com>
2020-02-21 22:37:05 +0100

    feature(dateUtils)

--- src/components/cv/Introduction.tsx
+++ src/components/cv/Introduction.tsx
@@ -1,9 +1,9 @@
+import { today } from "@/utils/dateUtils";
 import introductionData from "@content/cv/introduction.json";
 import React from "react";
 
 const getAge = (): string => {
   var firstYear = new Date(2011, 1, 1);
-  var today = new Date();
 
   return (today.getFullYear() - firstYear.getFullYear()).toString();
 };
