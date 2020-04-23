5be2983cb8a830f9dbbb7e17833bbbb65d2b41ea
CHARDEVEL AXEL <axel.chardevel.partner@decathlon.com>
2020-04-23 14:35:59 +0200

    feature(github-card): move style in css

--- src/components/cv/GithubCard.tsx
+++ src/components/cv/GithubCard.tsx
@@ -3,26 +3,11 @@ import React from "react";
 import { DiGithubFull } from "react-icons/di";
 import "src/styles/github-card.css";
 
-const style = {
-  display: "flex",
-  backgroundColor: "#fff",
-  border: "1px solid #d1d5da",
-  borderRadius: "3px",
-  padding: "0.9rem",
-  marginTop: "1.45rem",
-  marginBottom: "1.45rem",
-};
-
-const imageLinkStyle = {
-  marginLeft: "0.2rem",
-  marginTop: "-0.7rem",
-};
-
 const GithubCard = () => (
-  <div style={style}>
-    <p style={{ height: "33px" }}>{githubData.content.intro}</p>
+  <div className="github-card-container">
+    <p className="github-card-intro">{githubData.content.intro}</p>
     <a
-      style={imageLinkStyle}
+      className="github-card-link"
       href={`${githubData.content.link}/${githubData.content.profile}`}
     >
       <DiGithubFull className="github-icon" size={52} />
