23ab292ccc859722904ec526fe7461b281ddb0ef
CHARDEVEL AXEL <axel.chardevel.partner@decathlon.com>
2020-04-26 13:13:14 +0200

    feature(languages): I18n translation

--- src/components/cv/Languages.tsx
+++ src/components/cv/Languages.tsx
@@ -1,20 +1,24 @@
 import LanguageItem from "@components/cv/LanguageItem";
 import languagesData from "@content/cv/languages.json";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import "src/styles/languages.css";
 import CvCard from "./CvCard";
 
-const Languages = () => (
-  <div>
-    <h2>{languagesData.title}</h2>
-    <CvCard childrenPosition="center">
-      <div className="languages-container">
-        {languagesData.content.map((item, index) => {
-          return <LanguageItem key={`${item.language}-${index}`} item={item} />;
-        })}
-      </div>
-    </CvCard>
-  </div>
-);
+const Languages = () => {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <h2>{t(languagesData.title)}</h2>
+      <CvCard childrenPosition="center">
+        <div className="languages-container">
+          {languagesData.content.map((item, index) => (
+            <LanguageItem key={`${item.language}-${index}`} item={item} />
+          ))}
+        </div>
+      </CvCard>
+    </div>
+  );
+};
 
 export default Languages;
