a91bdd282dcdd10da2a407e68dafc5c8900c8ddd
Axel Chardevel <axel.chardevel.partner@decathlon.com>
2020-04-27 12:08:51 +0200

    feature(Circleci project setup) (#26)
    * Add .circleci/config.yml

--- src/scripts/create-cname.js
+++ src/scripts/create-cname.js
@@ -1,16 +1,13 @@
 var fs = require("fs");
 const path = require("path");
 
-const pathToFile = path.join(__dirname, "CNAME");
-const pathToNewDestination = path.join(__dirname, "../../public", "CNAME");
+const pathToFile = path.join(__dirname, "../../public/", "CNAME");
 
-fs.writeFile(pathToFile, "www.chardevel.com", function(err) {
+const errorOrSuccess = (err, success) => {
   if (err) throw err;
-  console.log("File is created successfully.");
-});
+  console.log(success);
+};
 
-fs.copyFile(pathToFile, pathToNewDestination, err => {
-  if (err) throw err;
-
-  console.log("CNAME was copied to public folder");
+fs.writeFile(pathToFile, "www.chardevel.com", err => {
+  errorOrSuccess(err, "File is created successfully.");
 });
