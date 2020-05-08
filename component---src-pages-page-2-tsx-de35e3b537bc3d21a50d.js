a101c09594b8b84ff22d54f5ffdf031a5ae12f24
CHARDEVEL AXEL <axel.chardevel.partner@decathlon.com>
2020-05-08 12:37:46 +0200

    Updates

---
+++ component---src-pages-page-2-tsx-de35e3b537bc3d21a50d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"+xhR":function(e,t,a){"use strict";a.r(t);var n=a("soUV"),o=a("hizP"),l=a("Wbzz"),c=a("q1tI"),r=a.n(c);t.default=function(){return r.a.createElement(n.a,null,r.a.createElement(o.a,{title:"Page two"}),r.a.createElement("h1",null,"Hi from the second page"),r.a.createElement("p",null,"Welcome to page 2"),r.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-de35e3b537bc3d21a50d.js.map
