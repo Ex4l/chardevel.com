5628bcab0adc39b612e4a7c4fb2e02dbc78a9515
Axel Chardevel <chardevel.a@gmail.com>
2021-07-14 00:23:45 +0200

    feature(table skills): use mui instead of html (#72)
    fixes #64

--- src/components/cv/skills/SkillsTable.tsx
+++ src/components/cv/skills/SkillsTable.tsx
@@ -1,3 +1,5 @@
+import useWindowSize from "@/hooks/useWindowSize";
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, withStyles } from "@material-ui/core";
 import { newline } from "@utils/jsxUtils";
 import React from "react";
 import { useTranslation } from "react-i18next";
@@ -9,62 +11,66 @@ interface IProps {
 }
 
 const SkillsTable = ({ data: { title, content } }: IProps) => {
-  const cellStyle = { textAlign: "center" as "center", fontSize: "smaller" };
   const starColor = "#FDE74C";
 
   const { t } = useTranslation();
 
+  const { width } = useWindowSize();
+
+  const getFontSize = (width: number): string => {
+    if (width <= 600) return "0.575rem"
+    if (width > 600 && width <= 800) return "0.650rem"
+    return "0.875rem"
+  }
+
+  const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: "#f6f8fa",
+  },
+  body: {
+    fontSize: getFontSize(width),
+    fontFamily: "georgia, serif",
+    lineHeight: "1.25rem"
+  },
+  }))(TableCell);
+
   return (
     <div>
       <h2>{t(title)}</h2>
-      <table>
-        <tbody>
-          {content.skills.map((skill, index) => {
-            const firstLine = index === 0;
-
-            return (
-              <tr key={`${skill.name}-${index}`}>
-                <td style={cellStyle}>
-                  <h5 className="skill-table-title">{t(skill.name)}</h5>
-                </td>
-                <td style={cellStyle}>
-                  {firstLine ? (
-                    <h5 className="skill-table-title">
-                      {newline(t(skill.notions))}
-                      <BsStarFill color={starColor} />
-                    </h5>
-                  ) : (
-                    newline(t(skill.notions))
-                  )}
-                </td>
-                <td style={cellStyle}>
-                  {firstLine ? (
-                    <h5 className="skill-table-title">
-                      {newline(t(skill.practice))}
-                      <BsStarFill color={starColor} />
-                      <BsStarFill color={starColor} />
-                    </h5>
-                  ) : (
-                    newline(t(skill.practice))
-                  )}
-                </td>
-                <td style={cellStyle}>
-                  {firstLine ? (
-                    <h5 className="skill-table-title">
-                      {newline(t(skill.mastery))}
-                      <BsStarFill color={starColor} />
-                      <BsStarFill color={starColor} />
-                      <BsStarFill color={starColor} />
-                    </h5>
-                  ) : (
-                    newline(t(skill.mastery))
-                  )}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <TableContainer>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell></StyledTableCell>
+              <StyledTableCell className="skill-table-title" align="center">
+                <h5>{t("skills.levels.notions")}</h5>
+                <BsStarFill color={starColor} />
+              </StyledTableCell>
+              <StyledTableCell className="skill-table-title" align="center">
+                <h5>{t("skills.levels.practice")}</h5>
+                <BsStarFill color={starColor} />
+                <BsStarFill color={starColor} />
+              </StyledTableCell>
+              <StyledTableCell className="skill-table-title" align="center">
+                <h5>{t("skills.levels.mastery")}</h5>
+                <BsStarFill color={starColor} />
+                <BsStarFill color={starColor} />
+                <BsStarFill color={starColor} />
+              </StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {content.skills.map((skill) => (
+            <TableRow key={skill.name}>
+              <StyledTableCell align="center"><h5 className="skill-table-title">{t(skill.name)}</h5></StyledTableCell>
+              <StyledTableCell align="center">{newline(t(skill.notions))}</StyledTableCell>
+              <StyledTableCell align="center">{newline(t(skill.practice))}</StyledTableCell>
+              <StyledTableCell align="center">{newline(t(skill.mastery))}</StyledTableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   );
 };
